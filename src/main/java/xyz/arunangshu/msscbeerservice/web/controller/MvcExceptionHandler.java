f67c8b28d91dae103f803c29060e8338caf5b87b
Arunangshu Biswas <arunangshubsws@gmail.com>
2022-03-19 16:32:30 +0530

    Added validation & error handling to controller, closes #9

---
+++ src/main/java/xyz/arunangshu/msscbeerservice/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,25 @@
+package xyz.arunangshu.msscbeerservice.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> validationErrorHandler(ConstraintViolationException ex) {
+        List<String> errors = ex.getConstraintViolations()
+                .stream()
+                .map(ConstraintViolation::getMessage)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
