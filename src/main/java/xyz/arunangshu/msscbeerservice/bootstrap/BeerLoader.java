7954690ff7a8098576deb733d40125f362da8c70
Arunangshu Biswas <arunangshubsws@gmail.com>
2022-04-03 10:19:30 +0530

    Updated BeerLoader to load data when empty, #18

--- src/main/java/xyz/arunangshu/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/xyz/arunangshu/msscbeerservice/bootstrap/BeerLoader.java
@@ -1,13 +1,16 @@
 package xyz.arunangshu.msscbeerservice.bootstrap;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import xyz.arunangshu.msscbeerservice.domain.Beer;
 import xyz.arunangshu.msscbeerservice.repositories.BeerRepository;
+import xyz.arunangshu.msscbeerservice.web.model.BeerStyleEnum;
 
 import java.math.BigDecimal;
 
 @Component
+@RequiredArgsConstructor
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
@@ -16,42 +19,40 @@ public class BeerLoader implements CommandLineRunner {
 
     private final BeerRepository beerRepository;
 
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
-
     @Override
     public void run(String... args) {
-        loadBeerObjects();
+        if(beerRepository.count() == 0 ) {
+            loadBeerObjects();
+        }
     }
 
     private void loadBeerObjects() {
         if (beerRepository.count() == 0) {
             beerRepository.save(Beer.builder()
                     .beerName("Mango Bobs")
-                    .beerStyle("IPA")
-                    .upc(BEER_1_UPC)
-                    .price(new BigDecimal("12.95"))
-                    .quantityToBrew(200)
+                    .beerStyle(BeerStyleEnum.IPA.name())
                     .minOnHand(12)
+                    .quantityToBrew(200)
+                    .price(new BigDecimal("12.95"))
+                    .upc(BEER_1_UPC)
                     .build());
 
             beerRepository.save(Beer.builder()
                     .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
-                    .upc(BEER_2_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .quantityToBrew(200)
+                    .beerStyle(BeerStyleEnum.PALE_ALE.name())
                     .minOnHand(12)
+                    .quantityToBrew(200)
+                    .price(new BigDecimal("12.95"))
+                    .upc(BEER_2_UPC)
                     .build());
 
             beerRepository.save(Beer.builder()
-                    .beerName("No Hammers On The Bar")
-                    .beerStyle("PALE_ALE")
-                    .upc(BEER_3_UPC)
-                    .price(new BigDecimal("11.95"))
-                    .quantityToBrew(200)
+                    .beerName("Pinball Porter")
+                    .beerStyle(BeerStyleEnum.PALE_ALE.name())
                     .minOnHand(12)
+                    .quantityToBrew(200)
+                    .price(new BigDecimal("12.95"))
+                    .upc(BEER_3_UPC)
                     .build());
         }
     }
